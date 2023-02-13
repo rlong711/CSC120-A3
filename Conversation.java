64d1079541eb9f165b98e62e8dab7e07dd00c7d7
rlong711 <you@example.com>
2023-02-13 18:05:28 -0500

    First Attempt

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,25 @@
-class Conversation {
+import javax.swing.UIClientPropertyKey;
+import java.util.Scanner;
 
-  public static void main(String[] arguments) {
-    System.out.print("Hi there!! ")
+public class Conversation {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello! and Welcome to the chat-box!!"); 
+  
+    System.out.println("How many rounds of conversation would you like? ");
+  
+    Double Rounds; 
+    Scanner in; 
+
+    in = new Scanner(System.in);
+    System.out.println("Enter how many rounds of conversation you would like: ");
+    Rounds = in.nextDouble(); 
+    System.exit(0);
+    
   }
+
+  
 }
+
+
+
